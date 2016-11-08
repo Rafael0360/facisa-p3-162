be4e7596f0cd640d522e4751f42c66bfba0569fe
rafael0360 <rafaeloliveiratfj.360@gmail.com>
2016-11-08 17:04:29 -0300

    Compativel com linux/windows

--- Sorting-Algorithms/src/br/facisa/p3/ed/main/Main.java
+++ Sorting-Algorithms/src/br/facisa/p3/ed/main/Main.java
@@ -1,5 +1,6 @@
 package br.facisa.p3.ed.main;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import br.facisa.p3.ed.fileHandling.File;
@@ -8,34 +9,42 @@ import br.facisa.p3.ed.sortingAlgorithms.QuickSort;
 import br.facisa.p3.ed.sortingAlgorithms.RadixSort;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		String file = "./txt/in.txt";
+	public static void main(String[] args) throws IOException,FileNotFoundException {
 		int[] array;
-		System.out.println("unordered list:");
-		array = File.getTextTxt(file);
-		for (int x: array){
-			System.out.print(x + " ");
-		}
-		System.out.println("\n");
-		System.out.println("Bubble Sort");
-		BubbleSort.sort(array);
-		for (int x: array){
-			System.out.print(x + " ");
-		}
+		String file = "./txt/in.txt";
+		try{
+			array = File.getTextTxt(file);
+		}catch(FileNotFoundException e){
+			file = "//txt//in.txt";
+		}finally{
 		
-		array = File.getTextTxt(file);
-		System.out.println("\n");
-		System.out.println("Quick Sort");
-		QuickSort.sort(array, 0, array.length-1);
-		for (int x: array){
-			System.out.print(x + " ");
-		}
-		array = File.getTextTxt(file);
-		System.out.println("\n");
-		System.out.println("Radix Sort");
-		RadixSort.sort(array);
-		for (int x: array){
-			System.out.print(x + " ");
+			System.out.println("unordered list:");
+			array = File.getTextTxt(file);
+			for (int x: array){
+				System.out.print(x + " ");
+			}
+			System.out.println("\n");
+			System.out.println("Bubble Sort");
+			BubbleSort.sort(array);
+			for (int x: array){
+				System.out.print(x + " ");
+			}
+			
+			array = File.getTextTxt(file);
+			System.out.println("\n");
+			System.out.println("Quick Sort");
+			QuickSort.sort(array, 0, array.length-1);
+			for (int x: array){
+				System.out.print(x + " ");
+			}
+			array = File.getTextTxt(file);
+			System.out.println("\n");
+			System.out.println("Radix Sort");
+			RadixSort.sort(array);
+			for (int x: array){
+				System.out.print(x + " ");
+	
+			}
 		}
 	}
 }
